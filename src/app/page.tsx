5f812b66297fe6f2b0a2f0c7775a28c6bf57f8ee
JianFengH <707156268@qq.com>
2023-11-04 06:12:50 +0800

    add Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,10 @@
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link, Button } from "@nextui-org/react";
-import Image from "next/image";
+import Navbar from '@/components/Navbar'
 
 export default function Home() {
-  return (
+  return <>
+    <Navbar current='' />
     <main className="">
-      <Navbar>
-        <NavbarBrand>
-          <Image
-            src="/logo.svg"
-            alt="Yaka Logo"
-            width={100}
-            height={24}
-            priority
-          />
-        </NavbarBrand>
-        <NavbarContent className="hidden sm:flex gap-4" justify="center">
-          <NavbarItem isActive>
-            <Link color="foreground" href="#">Swap</Link>
-          </NavbarItem>
-          <NavbarItem>
-            <Link color="foreground" href="#">Dashboard</Link>
-          </NavbarItem>
-          <NavbarItem>
-            <Link color="foreground" href="#">Liquidity</Link>
-          </NavbarItem>
-        </NavbarContent>
-        <NavbarContent className="hidden sm:flex" justify="end">
-          <NavbarItem>
-            <Button as={Link} color="primary" href="" variant="flat">Connect</Button>
-          </NavbarItem>
-        </NavbarContent>
-        <NavbarContent className="sm:hidden" justify="end">
-          <NavbarMenuToggle />
-        </NavbarContent>
-        <NavbarMenu>
-          <NavbarMenuItem>
-            <Link color="foreground" className="w-full" href="#" size="lg">Swap</Link>
-          </NavbarMenuItem>
-          <NavbarMenuItem>
-            <Link color="foreground" className="w-full" href="#" size="lg">Dashboard</Link>
-          </NavbarMenuItem>
-          <NavbarMenuItem>
-            <Link color="foreground" className="w-full" href="#" size="lg">Liquidity</Link>
-          </NavbarMenuItem>
-        </NavbarMenu>
-      </Navbar>
-      <div className="container"></div>
+
     </main>
-  )
+  </>
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,56 @@
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link, Button } from "@nextui-org/react";
+import Image from "next/image";
+
+export default function Home({ current }: { current: string }) {
+  const menus = [
+    { name: 'Swap', href: '#' },
+    { name: 'Dashboard', href: '#' },
+    { name: 'Liquidity', href: '#' },
+  ];
+
+  return (
+    <Navbar>
+      <NavbarBrand>
+        <Image
+          src="/logo.svg"
+          alt="Yaka Logo"
+          width={100}
+          height={24}
+          priority
+        />
+      </NavbarBrand>
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        {menus.map((item) => (
+          <NavbarItem key={item.name} isActive={current === item.name}>
+            <Link
+              color={current === item.name ? 'primary' : 'foreground'}
+              href={item.href}>
+              {item.name}
+            </Link>
+          </NavbarItem>
+        ))}
+      </NavbarContent>
+      <NavbarContent className="hidden sm:flex" justify="end">
+        <NavbarItem>
+          <Button as={Link} color="primary" href="" variant="flat">Connect</Button>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent className="sm:hidden" justify="end">
+        <NavbarMenuToggle />
+      </NavbarContent>
+      <NavbarMenu>
+        {menus.map((item) => (
+          <NavbarMenuItem key={item.name}>
+            <Link
+              color={current === item.name ? 'primary' : 'foreground'}
+              href={item.href}
+              className="w-full"
+              size="lg">
+              {item.name}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  )
+}
