b91c1197b00d8058823740488b3416834939e424
JianFengH <707156268@qq.com>
2023-11-04 08:54:11 +0800

    remove google Inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
 import { Providers } from './providers'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'YAKA',
@@ -17,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
-      <body className={inter.className}>
+      <body>
         <Providers>
           <div className="box-border min-h-screen flex flex-col">
             <div className="grow">{children}</div>
