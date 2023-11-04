f4731103cffad7147267ed8d6e5fe4e22865e527
JianFengH <707156268@qq.com>
2023-11-04 14:12:05 +0800

    add social media

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import { Providers } from './providers'
+import { Link } from '@nextui-org/react'
+import Image from 'next/image'
 
 export const metadata: Metadata = {
   title: 'YAKA',
@@ -18,10 +20,45 @@ export default function RootLayout({
         <Providers>
           <div className="box-border min-h-screen flex flex-col">
             <div className="grow">{children}</div>
-            <footer className="grow-0 shrink-0 w-full max-w-5xl mx-auto px-6 py-2">
+            <footer className="grow-0 shrink-0 w-full max-w-5xl mx-auto px-6 py-2 flex justify-between">
               <small>
-                Copyright © 2023 YAKA. All Rights Reserved.
+                Copyright © 2023 YAKA
               </small>
+              <ul className='flex items-center gap-2'>
+                <li>
+                  <Link href="https://twitter.com/YakaFinance" target='_blank'>
+                    <Image
+                      src="/twitter.svg"
+                      alt="Yaka twitter"
+                      width={24}
+                      height={24}
+                      priority
+                    />
+                  </Link>
+                </li>
+                <li>
+                  <Link href="https://discord.gg/tVjEfpMw" target='_blank'>
+                    <Image
+                      src="/discord.svg"
+                      alt="Yaka discord"
+                      width={24}
+                      height={24}
+                      priority
+                    />
+                  </Link>
+                </li>
+                <li>
+                  <Link href="https://yaka.gitbook.io/yaka-finance" target='_blank'>
+                    <Image
+                      src="/docs.svg"
+                      alt="Yaka docs"
+                      width={24}
+                      height={24}
+                      priority
+                    />
+                  </Link>
+                </li>
+              </ul>
             </footer>
           </div>
         </Providers>
